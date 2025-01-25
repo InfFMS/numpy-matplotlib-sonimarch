b3c5d30f49e8e21b095873af121e4544baecbd04
Марченко Софья Константиновна <s.marchenko1@main.nsu.ru>
2025-01-25 14:16:50 +0700

    кубик, сапер"

--- kubick.py
+++ kubick.py
@@ -7,12 +7,31 @@
 # Визуализируйте результаты в виде гистограммы.
 
 import matplotlib.pyplot as plt
-import numpy as np
 from random import randint
 
 
+
+s_cnt = []
+p = 0
+cnt = 0
 s = []
 for i in range(1000):
     s.append(randint(1, 6))
+    if s[i] == p:
+        cnt += 1
+    else:
+        s_cnt.append(cnt)
+        cnt = 0
+    p = s[i]
 
-print(s.count(1))
+
+for i in range(1, 7):
+    print(f'{i} выпало {s.count(i)} раз, вероятность - {s.count(i) / 10}%')
+
+print(f'Максимальное количество пдорят выпавших одинаковых значений {max(s_cnt)}')
+
+
+cat = [str(i) for i in range(1, 7)]
+val = [s.count(i) for i in range(1, 7)]
+plt.bar(cat, val, color='orange')
+plt.show()
--- mineswipper.py
+++ mineswipper.py
@@ -7,3 +7,71 @@
 # Само поле (где мины выделены красным).
 # Поле с числами, где указано количество мин вокруг каждой клетки (для наглядности).
 #
+import matplotlib.pyplot as plt
+import numpy as np
+from random import randint
+
+
+s = set()
+d1 = np.zeros((10, 10))
+d2 = np.zeros((10, 10))
+
+for i in range(10):
+    for j in range(10):
+        d2[i][j] += ((i + j) % 2) * 0.1
+
+
+while len(s) < 15:
+    s.add((randint(0, 9),randint(0, 9)))
+
+for i in s:
+    d1[i] = -1
+    d2[i] = 1
+
+fig = plt.figure(figsize = (10,5))
+plt.subplot(1,2,1)
+plt.imshow(d2, cmap='coolwarm')
+
+
+plt.subplot(1,2,2)
+cnt = 0
+for i in range(10):
+    for j in range(10):
+        if d2[i][j] != 1:
+            if i > 0:
+                if d1[i - 1][j] == -1:
+                    cnt += 1
+                if j > 0:
+                    if d1[i - 1][j - 1] == -1:
+                        cnt += 1
+                if j < 9:
+                    if d1[i - 1][j + 1] == -1:
+                        cnt += 1
+            if i < 9:
+                if d1[i + 1][j] == -1:
+                    cnt += 1
+                if j > 0:
+                    if d1[i + 1][j - 1] == -1:
+                        cnt += 1
+                if j < 9:
+                    if d1[i + 1][j + 1] == -1:
+                        cnt += 1
+            if j > 0:
+                if d1[i][j - 1] == -1:
+                    cnt += 1
+            if j < 9:
+                if d1[i][j + 1] == -1:
+                    cnt += 1
+            plt.text(j, i, cnt)
+
+            d1[i][j] = cnt
+            cnt = 0
+        else:
+            plt.text(j, i, 'M')
+
+
+
+
+plt.imshow(d1, cmap='Reds')
+
+plt.show()
